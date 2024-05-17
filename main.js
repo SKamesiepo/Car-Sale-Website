94cb3d7096a5a4824f586be117b12036fe3e3b45
SKamesiepo <skamesiepo@gmail.com>
2024-05-17 06:19:08 +0200

    Completed responsive sizing for mobile, tablet devices

--- main.js
+++ main.js
@@ -7,3 +7,13 @@ let header = document.querySelector('header');
 window.addEventListener('scroll', () => {
     header.classList.toggle('shadow', window.scrollY > 0);
 });
+
+let menu = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = () => {
+    menu.classList.toggle('active');   
+}
+
+window.onscroll = () => {
+    menu.classList.remove('active');  
+}
